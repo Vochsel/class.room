d6337bf7a8de26bddf3ec579a82b69c358f46960
Ben Skinner <benjamin.skinner96@gmail.com>
2016-06-19 17:05:56 +1000

    Syntax Highlighting, and tabs for files

--- public/scripts/socket.js
+++ public/scripts/socket.js
@@ -3,24 +3,56 @@ var server_port = 8000;
 
 var socket = new WebSocket("ws://" + server_ip + ":" + server_port);
 
-var doc;
+var editor;
+
+var docs = {};
+
+var currentTab = "";
+var tabs = document.getElementById("file_nav");
+
+function addTab(file) {
+	file_nav.innerHTML += "<li onclick='changeTab(`" + file.name + "`)'>" + file.name + "</li>";
+	docs[file.name] = {lang: file.lang, contents: file.contents};
+	changeTab(file.name);
+}
+
+function changeTab(name) {
+	currentTab = name;
+	editor.setOption("mode", docs[currentTab].lang);
+	editor.setValue(docs[currentTab].contents);
+}
+
+function editTab(name, newContents) {
+	docs[name].contents = newContents;
+}
+
+function updateTab() {
+	editor.setOption("mode", docs[currentTab].lang);
+	editor.setValue(docs[currentTab].contents);	
+}
 
 function Setup() {
-	doc = document.getElementById("code");
-	/*var editor = CodeMirror.fromTextArea(document.getElementById("editor_textarea"), {
-	  lineNumbers: true
-	  mode: "text/javascript",
-	  matchBrackets: true
-	});*/
-
+	//doc = document.getElementById("code"); 
+	editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+	  lineNumbers: true,
+	  mode: "text/html",
+	  matchBrackets: true,
+	  readOnly: true,
+	  indentUnit:5
+	});
+	editor.setSize(1150, 600);
 }
 
 socket.onmessage = function(data) {
 	var dataStr = data.data.toString();
-	console.log("Received packet from server");
-	//console.log(data);
-	dataStr = dataStr.replace(/(\r\n|\n|\r)/gm, "<br />");
-	//dataStr = dataStr.replace(/(")/gm, "");
-	dataStr = dataStr.replace(/(\t)/gm, "&nbsp;&nbsp;&nbsp;&nbsp;");
-	doc.innerHTML = dataStr;
-}  
+	var dataObj = JSON.parse(dataStr); 
+	console.log("Received packet from server. File: " + dataObj.lang);  
+	
+	dataObj.file.lang = dataObj.lang;
+	
+	if(docs[dataObj.file.name] == undefined)
+		addTab(dataObj.file);
+	else
+		editTab(dataObj.file.name, dataObj.file.contents);
+	updateTab();
+}
